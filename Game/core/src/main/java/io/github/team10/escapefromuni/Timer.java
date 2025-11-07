09e7563d72cbab7d2322682389db957cf7bd6d64
chiiiii123 <161492942+chiiiii123@users.noreply.github.com>
2025-11-07 00:52:27 +0000

    Update Timer.java

--- Game/core/src/main/java/io/github/team10/escapefromuni/Timer.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/Timer.java
@@ -2,29 +2,28 @@ package io.github.team10.escapefromuni;
 
 public class Timer {
 
-    private float time;
+    private float timeLeft;
 
     public Timer() {
-        this.time = 0;
+        this.timeLeft = 300; // start at 300 seconds
     }
 
     public void update(float delta) {
-        time += delta;
-    }
-
-    public float getTime() {
-        return time;
+        timeLeft -= delta;
+        if (timeLeft < 0) {
+            timeLeft = 0;
+        }
     }
 
     public int getTimeSeconds() {
-        return (int) time;
+        return (int) timeLeft;
     }
 
-    public boolean hasReached(float seconds) {
-        return time >= seconds;
+    public boolean isFinished() {
+        return timeLeft <= 0;
     }
 
     public void reset() {
-        time = 0;
+        timeLeft = 300;
     }
 }
